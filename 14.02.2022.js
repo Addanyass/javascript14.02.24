9493fae7a33175a8aac3a381ee4a24ced85cfe6c
Dani$ <152904949+Addanyass@users.noreply.github.com>
2024-02-14 20:56:23 +0300

    Add files via upload

---
+++ 14.02.2022.js
@@ -0,0 +1,61 @@
+
+// ОБЪЕКТЫ:
+
+
+// // создаем пустой объект
+// var student=new Object();
+// // Добавляем свойство в объект используя обычный
+// // синтаксис массивов
+// // ["ключ"]
+// student["Name"] = "Vasya";
+// // вместо двойных кавычек можно использовать одинарные
+// student['Age'] = 23;
+// alert(student["Name"]);
+// alert(student['Age']);
+
+
+
+// // создали объект студента
+// var student = {                                     //все переменный пишутся либо с var или let
+//     name: "Daria",
+//     lastName: "Kislicina",
+//     age:23,
+//     address:{street:"Tiraspolskaya 5",
+//     city:"Odessa",
+//     country:"Ukraine"
+//     }
+//    };
+//    alert(student.lastName);
+//    alert(student.address.street);
+//    alert(student.address.city);
+
+
+
+
+// var rect={
+//     x:0,
+//     y:0,
+//     endX:10,
+//     endY:10
+//    };
+//    // в tempProperty будет попадать название свойства
+//    // такое как x,y,endX,endY
+//    for(var tempProperty in rect){
+//     // отображаем название свойства
+//     alert(tempProperty);
+//     // значение свойства
+//     alert(rect[tempProperty]);
+//    }
+
+//------------------------------------------------------------------------------------------------------------------
+
+// МАССИВЫ:
+
+
+
+// // создаём пустой массив
+// var arrayName = [];
+// // создаём массив с набором значений
+// var arrayName = [1,2,3,4,5]; 
+// console.log(arrayName[1]) // если захочу отедльно вывести цифру 2
+
